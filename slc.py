94867df3a9bdb6dbd27af31cdfbd6d4fe67b4030
xibahbah <xikeith0901@gmail.com>
2025-12-10 01:32:53 -0800

    Refactor union-find implementation using DisjointSetForest

--- slc.py
+++ slc.py
@@ -1,40 +1,59 @@
+class DisjointSetForest:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+
+    def make_set(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry:
+            return
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+
+
 def slc(graph, d, k):
-    class UF:
-        def __init__(self, items):
-            self.parent = {x: x for x in items}
-            self.rank = {x: 0 for x in items}
-        def find(self, x):
-            if self.parent[x] != x:
-                self.parent[x] = self.find(self.parent[x])
-            return self.parent[x]
-        def union(self, x, y):
-            rx, ry = self.find(x), self.find(y)
-            if rx == ry:
-                return False
-            if self.rank[rx] < self.rank[ry]:
-                self.parent[rx] = ry
-            elif self.rank[rx] > self.rank[ry]:
-                self.parent[ry] = rx
-            else:
-                self.parent[ry] = rx
-                self.rank[rx] += 1
-            return True
+    dsf = DisjointSetForest()
+    for node in graph.nodes:
+        dsf.make_set(node)
 
-    nodes = list(graph.nodes())
-    uf = UF(nodes)
-    edges = [(u, v) for u, v in graph.edges()]
-    edges.sort(key=d)
-    clusters = len(nodes)
+    edges = []
+    for u, v in graph.edges:
+        w = d((u, v))
+        edges.append((w, u, v))
 
-    for u, v in edges:
-        if clusters == k:
+    edges.sort(key=lambda x: x[0])
+
+    num_clusters = len(graph.nodes)
+
+    for w, u, v in edges:
+        if num_clusters <= k:
             break
-        if uf.union(u, v):
-            clusters -= 1
+        if dsf.find(u) != dsf.find(v):
+            dsf.union(u, v)
+            num_clusters -= 1
 
-    groups = {}
-    for node in nodes:
-        r = uf.find(node)
-        groups.setdefault(r, set()).add(node)
+    clusters_dict = {}
+    for node in graph.nodes:
+        root = dsf.find(node)
+        if root not in clusters_dict:
+            clusters_dict[root] = set()
+        clusters_dict[root].add(node)
 
-    return frozenset(frozenset(g) for g in groups.values())
+    clusters = frozenset(frozenset(nodes) for nodes in clusters_dict.values())
+    return clusters
