3d8771c20eac002f738ca1d3b89e8bc384c9b26f
xibahbah <xikeith0901@gmail.com>
2025-12-10 01:15:54 -0800

    Refactor slc function with UnionFind class

--- slc.py
+++ slc.py
@@ -1,17 +1,43 @@
-def slc(graph, d, k):
+class UnionFind:
+    def __init__(self, items):
+        self.parent = {x: x for x in items}
+        self.rank = {x: 0 for x in items}
 
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+        return True
+
+
+def slc(graph, d, k):
     nodes = list(graph)
     uf = UnionFind(nodes)
-    num_clusters = len(nodes)
     edges = list(graph.edges())
     edges.sort(key=d)
+    clusters = len(nodes)
+
     for u, v in edges:
-        if num_clusters == k:
+        if clusters == k:
             break
         if uf.union(u, v):
-            num_clusters -= 1
-    clusters = {}
+            clusters -= 1
+
+    groups = {}
     for node in nodes:
-        root = uf.find(node)
-        clusters.setdefault(root, set()).add(node)
-    return frozenset(frozenset(cluster) for cluster in clusters.values())
+        r = uf.find(node)
+        groups.setdefault(r, set()).add(node)
+
+    return frozenset(frozenset(g) for g in groups.values())
