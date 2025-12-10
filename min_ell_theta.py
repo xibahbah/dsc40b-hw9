5ee2be917aaad30e12ace8eb517146b48ff935ce
xibahbah <xikeith0901@gmail.com>
2025-12-10 01:14:53 -0800

    Implement functions for theta and ell minimization
    Added functions to compute and minimize ell for given data and colors.

--- min_ell_theta.py
+++ min_ell_theta.py
@@ -1,22 +1,66 @@
-class UnionFind:
-    def __init__(self, items):
-        self.parent = {x: x for x in items}
-        self.rank = {x: 0 for x in items}
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rx, ry = self.find(x), self.find(y)
-        if rx == ry:
-            return False
-        if self.rank[rx] < self.rank[ry]:
-            self.parent[rx] = ry
-        elif self.rank[rx] > self.rank[ry]:
-            self.parent[ry] = rx
+def learn_theta(data, colors):
+    max_blue = float('-inf')
+    min_red = float('inf')
+    for x, c in zip(data, colors):
+        if c == 'blue':
+            if x > max_blue:
+                max_blue = x
         else:
-            self.parent[ry] = rx
-            self.rank[rx] += 1
-        return True
+            if x < min_red:
+                min_red = x
+    return 0.5 * (max_blue + min_red)
+
+
+def compute_ell(data, colors, theta):
+    r = 0
+    b = 0
+    for x, c in zip(data, colors):
+        if c == 'red' and x <= theta:
+            r += 1
+        elif c == 'blue' and x > theta:
+            b += 1
+    return float(r + b)
+
+
+def minimize_ell(data, colors):
+    pairs = list(zip(data, colors))
+    pairs.sort()
+    n = len(pairs)
+    pref_red = [0] * (n + 1)
+    suf_blue = [0] * (n + 1)
+    for i in range(n):
+        pref_red[i+1] = pref_red[i] + (1 if pairs[i][1] == 'red' else 0)
+    for i in range(n - 1, -1, -1):
+        suf_blue[i] = suf_blue[i+1] + (1 if pairs[i][1] == 'blue' else 0)
+    best_L = float('inf')
+    best_theta = None
+    for i in range(n):
+        L = pref_red[i+1] + suf_blue[i+1]
+        if L < best_L:
+            best_L = L
+            if i == n - 1:
+                best_theta = pairs[i][0] + 1
+            else:
+                best_theta = 0.5 * (pairs[i][0] + pairs[i+1][0])
+    return best_theta
+
+
+def minimize_ell_sorted(data, colors):
+    n = len(data)
+    pref_red = [0] * (n + 1)
+    suf_blue = [0] * (n + 1)
+    for i in range(n):
+        pref_red[i+1] = pref_red[i] + (1 if colors[i] == 'red' else 0)
+    for i in range(n - 1, -1, -1):
+        suf_blue[i] = suf_blue[i+1] + (1 if colors[i] == 'blue' else 0)
+    best_L = float('inf')
+    best_theta = None
+    for i in range(n):
+        L = pref_red[i+1] + suf_blue[i+1]
+        if L < best_L:
+            best_L = L
+            if i == n - 1:
+                best_theta = data[i] + 1
+            else:
+                best_theta = 0.5 * (data[i] + data[i+1])
+    return best_theta
